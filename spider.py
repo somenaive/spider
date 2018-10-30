15cacccc1d4a3bcefec391a67db8d382b6345b7a
zzxin <571599138@qq.com>
2018-10-30 21:33:11 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,12 +1,13 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 import sys
 import io
-import ck
 
 # 登录后才能访问的网页
 
 def getHtml(url):
+    '''
     # 浏览器登录后得到的cookie，也就是刚才复制的字符串
     cookie_str = ck.ck
 
@@ -15,12 +16,12 @@ def getHtml(url):
     for line in cookie_str.split(';'):
         key, value = line.split('=', 1)
         cookies[key] = value
-
+    '''
     # 设置请求头
     headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
 
     # 在发送get请求时带上请求头和cookies
-    resp = requests.get(url, headers=headers, cookies=cookies)
+    resp = requests.get(url, headers=headers)
     return resp.content.decode('utf-8')
 
 def saveImg(imgSrc):
@@ -34,12 +35,16 @@ def saveImg(imgSrc):
 
 
 if __name__=='__main__':
-    url = 'https://www.zhihu.com/topic/20022251/hot'
-    html = getHtml(url)
+    url = "http://www.zhihu.com/api/v4/topics/20022251/feeds/essence?include=data%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Danswer%29%5D.target.content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Danswer%29%5D.target.is_normal%2Ccomment_count%2Cvoteup_count%2Ccontent%2Crelevant_info%2Cexcerpt.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Darticle%29%5D.target.content%2Cvoteup_count%2Ccomment_count%2Cvoting%2Cauthor.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Dpeople%29%5D.target.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Danswer%29%5D.target.annotation_detail%2Ccontent%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F%28target.type%3Danswer%29%5D.target.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Darticle%29%5D.target.annotation_detail%2Ccontent%2Cauthor.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Dquestion%29%5D.target.annotation_detail%2Ccomment_count&limit=10&offset=0"
+
+
+    js = getHtml(url)
+    js=json.loads(js)
+    html=js['data'][1]['target']['content']
     soup = BeautifulSoup(html, 'html.parser')
     src = []
     for im in soup.find_all('img'):
         src.append(im.attrs['src'])
+    print(src)
 
-    saveImg(src)
-
+    #saveImg(src)
